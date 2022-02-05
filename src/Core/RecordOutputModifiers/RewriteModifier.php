9ec1f1a934f0d31a68e92c0d067e2e9ab4120888
Kestutis Arlauskas <kestutisa@gmail.com>
2022-02-05 10:09:37 +0200

    Rewrite modifier tests with data provider

--- src/Core/RecordOutputModifiers/RewriteModifier.php
+++ src/Core/RecordOutputModifiers/RewriteModifier.php
@@ -8,19 +8,97 @@ use Vcg\ValueObject\Record;
 
 class RewriteModifier implements RecordModifierInterface
 {
+    private array $outputData;
+    private ?string $level1;
+    private ?string $level2;
+    private ?string $level3;
+
     public function apply(Record $record): void
     {
-        $outputData = $record->getOutputData();
-        foreach ($record->getRewriteItems() as $index => $value) {
-            [$level1, $level2, $level3] = explode('|', $index);
+        $this->outputData = $record->getOutputData();
 
-            if (!array_key_exists($level3, $outputData[$level1][$level2])) {
+        foreach ($record->getRewriteItems() as $index => $value) {
+            $this->populateLevels($index);
+
+            if ($this->canRewriteLevel2nd()) {
+                $this->rewriteLevel2nd($value);
                 continue;
             }
 
-            $outputData[$level1][$level2][$level3] = $value;
+            if ($this->canRewriteLevel3rd()) {
+                $this->rewriteLevel3rd($value);
+            }
         }
 
-        $record->setOutputData($outputData);
+        $record->setOutputData($this->outputData);
+    }
+
+    private function populateLevels(string $index): void
+    {
+        $this->clearLevels();
+
+        $levels = explode('|', $index);
+
+        $this->level1 = $levels[0] ?? null;
+        $this->level2 = $levels[1] ?? null;
+        $this->level3 = $levels[2] ?? null;
+    }
+
+    private function clearLevels(): void
+    {
+        $this->level1 = null;
+        $this->level2 = null;
+        $this->level3 = null;
+    }
+
+    private function canRewriteLevel2nd(): bool
+    {
+        return $this->hasLevel2nd()
+            && $this->isKeyExistLevel2nd()
+            && $this->isValueLevel2nd();
+    }
+
+    private function hasLevel2nd(): bool
+    {
+        return null !== $this->level1
+            && null !== $this->level2;
+    }
+
+    private function isKeyExistLevel2nd(): bool
+    {
+        return array_key_exists($this->level2, $this->outputData[$this->level1]);
+    }
+
+    private function isValueLevel2nd(): bool
+    {
+        return !is_array($this->outputData[$this->level1][$this->level2]);
+    }
+
+    private function rewriteLevel2nd($value): void
+    {
+        $this->outputData[$this->level1][$this->level2] = $value;
+    }
+
+    private function canRewriteLevel3rd(): bool
+    {
+        return $this->hasLevel3rd()
+            && $this->isKeyExistLevel3d();
+    }
+
+    private function hasLevel3rd(): bool
+    {
+        return null !== $this->level1
+            && null !== $this->level2
+            && null !== $this->level3;
+    }
+
+    private function isKeyExistLevel3d(): bool
+    {
+        return array_key_exists($this->level3, $this->outputData[$this->level1][$this->level2]);
+    }
+
+    private function rewriteLevel3rd($value): void
+    {
+        $this->outputData[$this->level1][$this->level2][$this->level3] = $value;
     }
 }
--- tests/Unit/Core/RecordOutputModifiers/RewriteModifierTest.php
+++ tests/Unit/Core/RecordOutputModifiers/RewriteModifierTest.php
@@ -10,34 +10,30 @@ use Vcg\ValueObject\Record;
 
 class RewriteModifierTest extends TestCase
 {
-    public function testApply(): void
+    /**
+     * @dataProvider applyDataProvider
+     */
+    public function testApply(array $rewriteItems, array $expectedOutputData): void
     {
         $record = new Record();
         $record->setOutputData($this->initialOutputData());
 
-        foreach ($this->createRewriteItems() as $key => $item) {
+        foreach ($rewriteItems as $key => $item) {
             $record->addRewriteItem($key, $item);
         }
 
         $recordModifier = new RewriteModifier();
         $recordModifier->apply($record);
 
-        $this->assertEquals($this->expectedOutputData(), $record->getOutputData());
+        $this->assertEquals($expectedOutputData, $record->getOutputData());
     }
 
-    public function testRewriteOnlyExistingKeys(): void
+    public function applyDataProvider(): array
     {
-        $record = new Record();
-        $record->setOutputData($this->initialOutputData());
-
-        foreach ($this->createNonExistingKeys() as $key => $item) {
-            $record->addRewriteItem($key, $item);
-        }
-
-        $recordModifier = new RewriteModifier();
-        $recordModifier->apply($record);
-
-        $this->assertEquals($this->initialOutputData(), $record->getOutputData());
+        return [
+            'regular' => [$this->createRewriteItems(), $this->expectedOutputData()],
+            'non existing keys' => [$this->createNonExistingKeys(), $this->initialOutputData()],
+        ];
     }
 
     private function initialOutputData(): array
@@ -71,32 +67,16 @@ class RewriteModifierTest extends TestCase
         ];
     }
 
-    private function createRewriteItems(): array
-    {
-        return [
-            'response|status|http_version' => '1.2',
-            'response|status|code' => '500',
-            'response|status|message' => 'error',
-            'response|headers|Cache-Control' => 'public',
-            'response|headers|Content-Length' => '293',
-            'response|headers|Content-Type' => 'text/xml; charset=utf-16',
-            'response|headers|Server' => 'Microsoft-IIS/8.0.3',
-            'response|headers|X-AspNet-Version' => '4.1.1',
-            'response|headers|X-Powered-By' => 'ASP',
-            'response|headers|Date' => 'Fri, 4 Feb 2022 08:17:39 GMT',
-        ];
-    }
-
     private function expectedOutputData(): array
     {
         return [
             'request' => [
-                'method' => 'POST',
-                'url' => 'http://127.0.0.1:8080/soap',
+                'method' => 'GET',
+                'url' => 'http://127.0.0.1:8888/soap',
                 'headers' => [
-                    'Host' => '127.0.0.1:8080',
-                    'Content-Type' => 'text/xml; charset=utf-8;',
-                    'SOAPAction' => 'http://tempuri.org/'
+                    'Host' => '127.0.0.1:8888',
+                    'Content-Type' => 'text/xml; charset=utf-16;',
+                    'SOAPAction' => 'https://tempuri.org/'
                 ]
             ],
             'response' => [
@@ -118,10 +98,39 @@ class RewriteModifierTest extends TestCase
         ];
     }
 
+    private function createRewriteItems(): array
+    {
+        return [
+            'request|method' => 'GET',
+            'request|url' => 'http://127.0.0.1:8888/soap',
+            'request|headers|Host' => '127.0.0.1:8888',
+            'request|headers|Content-Type' => 'text/xml; charset=utf-16;',
+            'request|headers|SOAPAction' => 'https://tempuri.org/',
+            'response|status|http_version' => '1.2',
+            'response|status|code' => '500',
+            'response|status|message' => 'error',
+            'response|headers|Cache-Control' => 'public',
+            'response|headers|Content-Length' => '293',
+            'response|headers|Content-Type' => 'text/xml; charset=utf-16',
+            'response|headers|Server' => 'Microsoft-IIS/8.0.3',
+            'response|headers|X-AspNet-Version' => '4.1.1',
+            'response|headers|X-Powered-By' => 'ASP',
+            'response|headers|Date' => 'Fri, 4 Feb 2022 08:17:39 GMT',
+        ];
+    }
+
     private function createNonExistingKeys(): array
     {
         return [
+            'request' => 'na',
+            'request|input' => 'na',
+            'request|headers|Content-length' => '0',
+            'request|headers|Input|Output' => 'na',
+            'response' => 'na',
+            'response|input' => 'na',
             'response|headers|message' => 'error',
+            'response|status|non-exist' => 'na',
+            'input' => 'na',
         ];
     }
 }
